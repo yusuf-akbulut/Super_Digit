e41a2ee0c8df466abf5e9ff3ac40d68fcb70d730
yusuf-akbulut <83082088+yusuf-akbulut@users.noreply.github.com>
2021-06-11 02:21:07 +0300

    Add files via upload

---
+++ SuperDigitFinder.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+//This program finds the super digit of any number.
+
+int sum1 , sum2;  //Global variables.
+
+void f(int n ,int k){
+
+int temp = n;	
+
+ //This loop finds the sum of digits of the given n.
+	while(n>0){
+	   
+	 int digit= n% 10;
+	   sum1+=digit; 
+	   n /= 10; 
+	}  
+	
+	
+	n=temp;
+	
+	if(k>1)    
+	   f(n,k-1);   //Recursive statement.It is used due to repetition factor.
+	 
+	else{    //Base case
+	
+    //These loops finds the super digit of the number.
+	do{
+        sum2 =0;
+        while(sum1>0){
+		  int digit= sum1%10;
+		  sum2 += digit;
+		  sum1/=10;   }
+        
+           sum1=sum2;
+           
+	} while(sum2>9);
+
+  }
+}
+
+int main() {
+
+//Local variables.n holds given the number and k holds given the repetiton factor.
+ int n ,k;
+
+//Read the number.
+  printf("Please enter a number (n=) : ");
+  scanf("%d",&n);   
+
+//Read the repetition factor.
+  printf("Please enter repetition factor (k=) : ");
+  scanf("%d",&k);
+
+        f(n,k);
+  
+//Some prints. 	  
+  printf("Super digit of number " ); 
+    
+	  while(k>0){
+	      
+        printf("%d",n);
+        k--; 
+  }     
+      printf(" is %d.",sum2 );      
+
+      return 0;
+}
+
